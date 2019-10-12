0b6c6885b2d041d5df282500f5bc3145af37ea55
sachin sav <47114417+sachinsav@users.noreply.github.com>
2019-10-13 00:44:38 +0530

    little update

--- getReady.php
+++ getReady.php
@@ -2,11 +2,11 @@
 $con=mysqli_connect("localhost","root","");
 $sql="create database user";
 $run=mysqli_query($con,$sql);
-$sql="use useree_2";
+$sql="use user";
 $run=mysqli_query($con,$sql);
 
 $sql="create table emails (email varchar(35) NOT NULL,message varchar(400) NOT NULL,eid int(11) PRIMARY KEY AUTO_INCREMENT)";
 $run=mysqli_query($con,$sql);
-$sql="create table posts (post_id int(11) AUTO_INCREMENT PRIMARY KEY,post_content varchar(400) NOT NULL,post_image varchar(255)  NOT NULL,title varchar(100) NOT NULL,date timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,name varchar(20) NOT NULL,type varchar(20) NOT NULL)";
+$sql="create table posts (post_id int(11) AUTO_INCREMENT PRIMARY KEY,post_content varchar(400) NOT NULL,post_image varchar(255)  NOT NULL,title varchar(100) NOT NULL,date timestamp DEFAULT CURRENT_TIMESTAMP,name varchar(20) NOT NULL,type varchar(20) NOT NULL)";
 $run=mysqli_query($con,$sql);
 ?>
